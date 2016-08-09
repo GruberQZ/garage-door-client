9ba60dc2e78552b87fb30d05f8aee5c31296fbfd
Blake Loewengruber <ambsneakysnack@gmail.com>
2016-08-09 12:20:33 -0400

    Initial GET and decode of API calls

--- TestAPI.py
+++ TestAPI.py
@@ -1,3 +1,12 @@
 # Testing the API and testing Git
 import json
 from pprint import pprint
+import urllib.request
+
+# Need to test API in range 6212-6215
+for i in range(6212,6216,1):
+    # Retrieve the JSON file
+    response = urllib.request.urlopen("http://devops-tutorial-1-jploewen-1945.mybluemix.net/garages/" + str(i))
+    output = response.read().decode('utf-8')
+    data = json.loads(output)
+    pprint(data)
