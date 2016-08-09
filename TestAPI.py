dbe2ad271b0e5adea2adf73e3e431ce4b7aebb14
Blake Loewengruber <ambsneakysnack@gmail.com>
2016-08-09 12:36:18 -0400

    Adding comments

--- TestAPI.py
+++ TestAPI.py
@@ -3,10 +3,13 @@ import json
 from pprint import pprint
 import urllib.request
 
-# Need to test API in range 6212-6215
+# Need to test APIs at /6212, /6213, /6214, and /6215
 for i in range(6212,6216,1):
     # Retrieve the JSON file
     response = urllib.request.urlopen("http://devops-tutorial-1-jploewen-1945.mybluemix.net/garages/" + str(i))
+    # Decode response with proper charset
     output = response.read().decode('utf-8')
+    # Load output string into JSON
     data = json.loads(output)
+    # Pretty print data to ensure it came in correctly
     pprint(data)
