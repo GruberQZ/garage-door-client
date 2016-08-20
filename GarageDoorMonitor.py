fb552c088eaec363bc00f8449010b194885ea45c
Blake <ambsneakysnack@gmail.com>
2016-08-19 21:05:00 -0400

    Added random API call extension

--- GarageDoorMonitor.py
+++ GarageDoorMonitor.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import json
 from pprint import pprint
 import urllib.request
+import random
 
 # Set numbering scheme to BCM
 GPIO.setmode(GPIO.BCM)
@@ -17,12 +18,17 @@ GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Need to get the current desired configuration from the cloud app
 # Retrieve the JSON file
-i = 6212
+i = 6211 + random.randint(1,4)
 response = urllib.request.urlopen("http://devops-tutorial-1-jploewen-1945.mybluemix.net/garages/" + str(i))
 # Decode response with proper charset
 output = response.read().decode('utf-8')
 # Load output string into JSON
 data = json.loads(output)
+
+# Get the current status of the door
+
+
+# Process the data that comes back
 
 # Clean up GPIO on exit
 GPIO.cleanup()
